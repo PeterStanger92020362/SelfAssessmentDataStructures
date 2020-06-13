3dce4dce4b5f87ce81891483711c46652073473f
PeterStanger92020362 <psta191@mywhitecliffe.com>
2020-06-13 16:22:56 +1200

    Completed second Data Structure scenario example

--- src/selfassessmentdatastructures/SelfAssessmentDataStructures.java
+++ src/selfassessmentdatastructures/SelfAssessmentDataStructures.java
@@ -8,7 +8,9 @@ package selfassessmentdatastructures;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.Stack;
 
 /**
  *
@@ -20,6 +22,8 @@ public class SelfAssessmentDataStructures {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        /*
         
         //Sets
                
@@ -38,6 +42,39 @@ public class SelfAssessmentDataStructures {
         
         System.out.println("Here is the same list, but only unique values using a Set data structure:");
         System.out.println(uniqueSet);
+        
+
+        */
+        
+        
+        
+        //Stacks
+        
+        Scanner entry = new Scanner(System.in);
+
+        Stack<String> userInputStack = new Stack<>();
+        
+        System.out.println("Enter some data and then type 'u' to remove last entered items");
+        while(true){
+            
+            String userInput = entry.next();
+
+            if (userInput.toLowerCase().equals("u")){
+                if (userInputStack.isEmpty()) {
+                    System.out.println("Nothing to remove?");
+                } else {
+                    System.out.println(userInputStack.pop() + " removed from user entry stack");
+                    System.out.println(userInputStack);
+                }
+            } else {
+                userInputStack.push(userInput);
+                System.out.println(userInputStack);                
+            }
+
+        }
+        
+        
+        
         
     }
     
